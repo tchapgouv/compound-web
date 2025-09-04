f8ad97e393595d2ed07090b95097db582f093961
MarcWadai <MarcWadai@users.noreply.github.com>
2025-09-04 10:10:47 +0000

    deploy: f1fde225102b9f62acc7b91b7dd405598b1c35f0

---
+++ assets/Separator.stories-Rdu918qj.js
@@ -0,0 +1,13 @@
+import{S as e}from"./Separator-CPvI8gw0.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-FGJkmGnF.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CgfFrydU.js";import"./index-BFVwPJal.js";import"./index-Brqd7pHr.js";import"./index-B2KKL2uI.js";import"./index-yMRBpD7g.js";import"./floating-ui.react-Ds7-_usg.js";import"./floating-ui.react-dom-Ct6bBtJ8.js";const y={title:"Separator",component:e,tags:["autodocs"],argTypes:{},args:{}},r={args:{kind:"primary"}},a={args:{kind:"secondary"}},o={args:{kind:"section"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "primary"
+  }
+}`,...r.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "secondary"
+  }
+}`,...a.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "section"
+  }
+}`,...o.parameters?.docs?.source}}};const k=["Primary","Secondary","Section"];export{r as Primary,a as Secondary,o as Section,k as __namedExportsOrder,y as default};
